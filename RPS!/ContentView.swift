96f1785ebf265031978767219b33f26e5477cb53
Jorge Pardo <hi@jorgepardo.dev>
2023-09-23 14:45:26 +0200

    Initial Commit

---
+++ RPS!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPS!
+//
+//  Created by Jorge Pardo on 23/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPS!/RPS_App.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_App.swift
+//  RPS!
+//
+//  Created by Jorge Pardo on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RPS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
